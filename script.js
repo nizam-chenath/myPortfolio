6dd172bd3664846fa2e6143cc79abd94006bd7ef
nizam-chenath <nizamstudymajliz@gmail.com>
2022-03-11 23:33:38 +0530

    footer added and projects page modified

--- script.js
+++ script.js
@@ -42,11 +42,20 @@ let tl6 = gsap.timeline({
         scrub: 1,
     },
 });
+let tl7 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".third-page",
+        start: "-80%",
+        end: "120%",
+        scrub: 1,
+    },
+});
 tl.fromTo('.sliding-text', { y: 0 }, { y: -400 });
 tl2.fromTo('.logo', {scale: 6},{scale:1, top: "2rem", left: "3rem", x:"50%", y: "50%"});
 tl4.fromTo('.square', {left: '70%'}, {left: '100%' , top: "10%", opacity: 0}, );
 tl5.fromTo('.firstImg', {left: '30%'}, {left: '100%' , opacity: 0}, );
 tl6.fromTo('.secondImg', {opacity: 0}, { opacity: 1}, );
+tl7.fromTo('.square2', {left: '10%',top: "80%"}, {left: '100%' , top: "0%", opacity: 0}, );
 
 
 
