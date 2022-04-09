ef0acd4be788e8703cf270913d613b6bcd49a6d8
nizam-chenath <nizamstudymajliz@gmail.com>
2022-04-09 23:17:05 +0530

    respnsive

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const navSlide = () =>{
   const nav = document.querySelector('.nav-links')
   const navLinks =  document.querySelectorAll('.nav-links li');
 
-
+console.log(window.innerWidth)
+console.log(window.innerWidth)
   burger.addEventListener('click',()=>{
       //toggle nav
       nav.classList.toggle('nav-active');
@@ -131,10 +132,19 @@ let tl15 = gsap.timeline({
         scrub: 1,
     },
 });
+let tl20 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".home",
+        start: "0%",
+        end: "40%",
+        scrub: 1,
+    },
+});
+
 
 tl.fromTo('.sliding-text', { y: 0 }, { y: -400 });
 tl2.fromTo('.logo', {scale: 6},{scale:1, top: "2rem", left: "3rem", x:"50%", y: "50%"});
-tl4.fromTo('.square', {left: '70%'}, {left: '100%' , top: "10%", opacity: 0}, );
+tl4.fromTo('.square', {left: '55%'}, {left: '100%' , top: "10%", opacity: 0}, );
 tl5.fromTo('.firstImg', {left: '30%'}, {left: '100%' , opacity: 0}, );
 tl6.fromTo('.secondImg', {opacity: 0}, { opacity: 1}, );
 tl7.fromTo('.square2', {left: '10%',top: "80%"}, {left: '100%' , top: "0%", opacity: 0}, );
@@ -145,6 +155,7 @@ tl10.fromTo('.project', { opacity: 0}, {  opacity: 1}, );
 tl11.fromTo('.intro1', { opacity: 0}, {  opacity: 1}, );
 tl12.fromTo('.intro2', { opacity: 0}, {  opacity: 1}, );
 tl15.fromTo('.blog-box', { opacity: 0}, {  opacity: 1}, );
+tl20.fromTo('.nav-links', { opacity: 0.9}, {  opacity: 0}, );
 
 
 
@@ -152,7 +163,7 @@ let tl3 = gsap.timeline({
     scrollTrigger: {
         trigger: ".home",
         start: "0%",
-        end: "100%",
+        end: "50%",
         scrub: 1,
         pin: true,
         pinSpacing: false,
