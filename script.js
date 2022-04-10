84205b0ef71621be3d7570c9ed56a9e339026d1b
nizam-chenath <nizamstudymajliz@gmail.com>
2022-04-10 21:37:00 +0530

    almost responsive

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const navSlide = () =>{
   const nav = document.querySelector('.nav-links')
   const navLinks =  document.querySelectorAll('.nav-links li');
 
-console.log(window.innerWidth)
-console.log(window.innerWidth)
+
+
   burger.addEventListener('click',()=>{
       //toggle nav
       nav.classList.toggle('nav-active');
@@ -24,6 +24,7 @@ console.log(window.innerWidth)
 }
 navSlide();
 
+console.log(window.innerWidth);
 let tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".home",
@@ -33,14 +34,27 @@ let tl = gsap.timeline({
     },
 });
 
-let tl2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".home",
-        start: "0%",
-        end: "60%",
-        scrub: 1,
-    },
-});
+// Logo 
+if(window.innerWidth < 700){
+    var tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".home",
+            start: "0%",
+            end: "110%",
+            scrub: 1,
+        },
+    });
+}else{
+
+    var tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".home",
+            start: "0%",
+            end: "60%",
+            scrub: 1,
+        },
+    });
+}
 
 
 
@@ -60,22 +74,48 @@ let tl5 = gsap.timeline({
         scrub: 1,
     },
 });
-let tl11 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".first-page",
-        start: "-20%",
-        end: "50%",
-        scrub: 1,
-    },
-});
-let tl6 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".second-page",
-        start: "-50%",
-        end: "20%",
-        scrub: 1,
-    },
-});
+//about us
+if(window.innerWidth<600){
+    var tl11 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".first-page",
+            start: "-60%",
+            end: "-40%",
+            scrub: 1,
+        },
+    });
+}else{
+
+    var tl11 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".first-page",
+            start: "-20%",
+            end: "50%",
+            scrub: 1,
+        },
+    });
+}
+//secondimage in aboutUs
+if(window.innerWidth < 600){
+    var tl6 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".second-page",
+            start: "-60%",
+            end: "20%",
+            scrub: 1,
+        },
+    });
+}else{
+
+    var tl6 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".second-page",
+            start: "-50%",
+            end: "20%",
+            scrub: 1,
+        },
+    });
+}
 let tl7 = gsap.timeline({
     scrollTrigger: {
         trigger: ".third-page",
@@ -84,30 +124,69 @@ let tl7 = gsap.timeline({
         scrub: 1,
     },
 });
-let tl8 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".contact-page",
-        start: "-20%",
-        end: "20%",
-        scrub: 1,
-    },
-});
-let tl13 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".contact-page",
-        start: "-50%",
-        end: "5%",
-        scrub: 1,
-    },
-});
-let tl9 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".third-page",
-        start: "-30%",
-        end: "30%",
-        scrub: 1,
-    },
-});
+//form
+if(window.innerWidth < 600){
+
+    var tl8 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".contact-page",
+            start: "-70%",
+            end: "-10%",
+            scrub: 1,
+        },
+    });
+}else{
+    var tl8 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".contact-page",
+            start: "-20%",
+            end: "20%",
+            scrub: 1,
+        },
+    });
+}
+if(window.innerWidth < 600){
+
+    var tl13 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".third-page",
+            start: "60%",
+            end: "80%",
+            scrub: 1,
+        },
+    });
+}else{
+    var tl13 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".contact-page",
+            start: "-50%",
+            end: "5%",
+            scrub: 1,
+        },
+    });
+}
+
+//card
+if(window.innerWidth < 600){
+    var tl9 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".third-page",
+            start: "-30%",
+            end: "-10%",
+            scrub: 1,
+        },
+    });
+}else{
+
+    var tl9 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".third-page",
+            start: "-30%",
+            end: "30%",
+            scrub: 1,
+        },
+    });
+}
 let tl10 = gsap.timeline({
     scrollTrigger: {
         trigger: ".third-page",
@@ -116,14 +195,27 @@ let tl10 = gsap.timeline({
         scrub: 1,
     },
 });
-let tl12 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".second-page",
-        start: "-20%",
-        end: "50%",
-        scrub: 1,
-    },
-});
+if(window.innerWidth  < 600){
+    
+    var tl12 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".second-page",
+            start: "-60%",
+            end: "-20%",
+            scrub: 1,
+        },
+    });
+}else{
+     
+    var tl12 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".second-page",
+            start: "-20%",
+            end: "50%",
+            scrub: 1,
+        },
+    });
+}
 let tl15 = gsap.timeline({
     scrollTrigger: {
         trigger: ".blog",
@@ -136,7 +228,7 @@ let tl20 = gsap.timeline({
     scrollTrigger: {
         trigger: ".home",
         start: "0%",
-        end: "40%",
+        end: "50%",
         scrub: 1,
     },
 });
@@ -149,24 +241,33 @@ tl5.fromTo('.firstImg', {left: '30%'}, {left: '100%' , opacity: 0}, );
 tl6.fromTo('.secondImg', {opacity: 0}, { opacity: 1}, );
 tl7.fromTo('.square2', {left: '10%',top: "80%"}, {left: '100%' , top: "0%", opacity: 0}, );
 tl8.fromTo('.form-section', {opacity: 0}, { opacity: 1}, );
-tl13.fromTo('.details-section', {opacity: 0}, { opacity: 1}, );
+//details section in form
+if(window.innerWidth < 600){
+
+    tl13.fromTo('.details-section', {opacity: 0, top: "50%"}, { opacity: 1, top: '10%'}, );
+}else{
+    tl13.fromTo('.details-section', {opacity: 0}, { opacity: 1}, );
+}
 tl9.fromTo('.card', { opacity: 0}, {  opacity: 1}, );
 tl10.fromTo('.project', { opacity: 0}, {  opacity: 1}, );
 tl11.fromTo('.intro1', { opacity: 0}, {  opacity: 1}, );
 tl12.fromTo('.intro2', { opacity: 0}, {  opacity: 1}, );
 tl15.fromTo('.blog-box', { opacity: 0}, {  opacity: 1}, );
-tl20.fromTo('.nav-links', { opacity: 0.9}, {  opacity: 0}, );
+if(window.innerWidth < 600){
+
+    tl20.fromTo('.nav-links', { opacity: 0.9}, {  opacity: 0}, );
+}
 
 
-
-let tl3 = gsap.timeline({
+ tl3 = gsap.timeline({
     scrollTrigger: {
         trigger: ".home",
         start: "0%",
-        end: "50%",
+        end: "300%",
         scrub: 1,
         pin: true,
         pinSpacing: false,
     },
 });
+
 
